93bc90a53f5dfd00db6a1774c930cb3f75870a55
denis-volkov <denis-not@ya.ru>
2020-04-17 22:22:07 +0300

    Урок 2. Доделал скраппинг сайта SJ.

--- les_2.py
+++ les_2.py
@@ -2,6 +2,18 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 
+
+def translit_to_en(string):
+    alfabet = legend = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z',
+                        'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+                        'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh',
+                        'щ': 'shch', 'ъ': 'y', 'ы': 'y', 'ь': "", 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+    string = string.lower()
+    for i, item in alfabet.items():
+        string = string.replace(i, item)
+    return string
+
+
 link_base = 'https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text='
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36 OPR/67.0.3575.137'}
 
@@ -40,6 +52,47 @@ while True:
     if not html.findAll('a', {'class': 'HH-Pager-Controls-Next'}):
         break
     page += 1
+
+# Скраппинг SJ
+link_base = 'https://russia.superjob.ru'
+job = job.replace('+', '-')
+job = translit_to_en(job)
+link = link_base + '/vakansii/'+ job + '.html'
+
+req = requests.get(link, headers=headers).text
+html = bs(req, 'lxml')
+end = '0'
+for i in html.findAll('span', {'class': '_3IDf-'}):
+        if i.text == 'Дальше':
+            break
+        end = i.text
+end = int(end)
+for page in range(end+1):
+    req = requests.get(f'{link}?page={page}', headers=headers).text
+    html = bs(req, 'lxml')
+    block_vacancy = html.findAll('div', {'class': 'acdxh GPKTZ _1tH7S'})
+    for i in block_vacancy:
+        vacancy = {'site': 'Super Job'}
+        data = i.find('a')
+        vacancy['ref'] = link_base + re.search('(?<=href=")[a-zA-Z0-9:\/.\-?=%]+', str(data)).group()
+        vacancy['name'] = data.text
+        vacancy['zp'] = [None, None, None]
+        data = i.find('span', {'class': '_3mfro _2Wp8I _31tpt f-test-text-company-item-salary PlM3e _2JVkc '
+                                        '_2VHxz'}).text
+        if data != 'По договорённости':
+            data = data.split()
+            vacancy['zp'][2] = data[-1]
+            if 'от' in data:
+                vacancy['zp'][0] = data[1] + data[2]
+            elif 'до' in data:
+                vacancy['zp'][1] = data[1] + data[2]
+            elif '—' in data:
+                vacancy['zp'][0] = data[0] + data[1]
+                vacancy['zp'][1] = data[3] + data[4]
+            else:
+                vacancy['zp'][0] = vacancy['zp'][1] = ''.join(data[:-1])
+
+        result.append(vacancy)
 
 for i in result:
     print('Вакансия:', i['name'])
